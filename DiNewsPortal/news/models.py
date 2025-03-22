2693e840cb44d5da1d1bd2fbbaf3591c354f227a
DianaDju <diana.dzhumashova@mail.ru>
2025-03-22 21:33:08 +0300

    models.py

--- DiNewsPortal/news/models.py
+++ DiNewsPortal/news/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Sum
-from django.db import models
-from django.contrib.auth.models import User
-from django.db.models import Sum
 
 
 class Author(models.Model):
